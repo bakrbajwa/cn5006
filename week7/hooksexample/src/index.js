877784e115e3f8b767e85b65e44a79c69536413f
bakrbajwa <abubakarbajwa055@gmail.com>
2025-12-16 15:00:39 +0000

    Add files via upload

---
+++ week7/hooksexample/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Hook_ControlledButtonState from '../../newapp/src/counter'
+
+import EmojeeCounter from '../../newapp/src/EmojeeCounters'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+ root.render(
+  <React.StrictMode>
+    <Hook_ControlledButtonState />
+    <EmojeeCounter pic='love'/>
+    <EmojeeCounter pic='sad'/>
+    <EmojeeCounter pic='happy'/>
+    <EmojeeCounter pic='like'/>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
